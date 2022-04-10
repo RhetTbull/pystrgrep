aeddd878e45ceb6d6e742aa96f7b5717f27b65b7
Rhet Turnbull <rturnbull@gmail.com>
2022-04-10 08:35:54 -0700

    Fixed printing for brackets

--- pystrgrep/__main__.py
+++ pystrgrep/__main__.py
@@ -8,15 +8,18 @@ import re
 from pathlib import Path
 from typing import List, Optional
 
+import rich.traceback
+import typer
 from rich.console import Console
 from rich.text import Text
-import typer
 
 from ._version import __version__
 
+rich.traceback.install()
+
 
 def get_str_value(node: ast.AST) -> str:
-    """Return string value of ast node if it is a string constant
+    """Return string value of ast node if it is a string constant, otherwise None
 
     Args:
         node: ast node
@@ -37,18 +40,36 @@ def print_match(
 ):
     """Print match from grep"""
     filename_str = f"{filename}:{node.lineno}:" if line_number else f"{filename}:"
+
     line = line[:-1] if line.endswith("\n") else line
 
+    # print the entire line, breaking it into three parts:
+    # line_pre (before the node), line_value (the node), and line_post (after the node)
     line_pre = line[: node.col_offset]
     line_post = line[node.end_col_offset :]
     line_pre = escape_brackets(line_pre)
     line_post = escape_brackets(line_post)
 
-    flags = re.IGNORECASE if ignore_case else 0
+    # build up the value to print while escaping brackets (for rich)
+    # and highlighting the match
     line_value = line[node.col_offset : node.end_col_offset]
-    line_values = re.split(pattern, line_value, flags=flags)
-    line_values = [escape_brackets(v) for v in line_values]
-    line_value = f"[bold red]{pattern}[/]".join(line_values)
+
+    # find all matches in the line_value
+    flags = re.IGNORECASE if ignore_case else 0
+    match_spans = [
+        match.span() for match in re.finditer(pattern, line_value, flags=flags)
+    ]
+    new_line_value = ""
+    last_end = 0
+    for start, end in match_spans:
+        new_line_value += escape_brackets(line_value[last_end:start])
+        new_line_value += (
+            f"[bold red]{escape_brackets(line_value[start:end])}[/bold red]"
+        )
+        last_end = end
+    if last_end != len(line_value):
+        new_line_value += escape_brackets(line_value[last_end:])
+    line_value = new_line_value
 
     console = Console(highlight=False)
     console.print(f"{filename_str}{line_pre}{line_value}{line_post}")
@@ -88,6 +109,15 @@ def grep(
     """grep for PATTERN in python string constants inside FILE(s)"""
 
     pattern = pattern.replace("\\\\", "\\")
+    flags = re.IGNORECASE if ignore_case else 0
+    try:
+        regex = re.compile(pattern, flags=flags)
+    except re.error as e:
+        typer.secho(
+            f"Invalid regular expression: '{pattern}' ({e})", fg=typer.colors.RED
+        )
+        raise typer.Exit(1) from e
+
     for f in files:
         with open(f, "r") as fh:
             lines = fh.readlines()
@@ -95,11 +125,7 @@ def grep(
 
         for node in ast.walk(ast.parse(contents)):
             if value := get_str_value(node):
-                if re.search(
-                    pattern,
-                    value,
-                    flags=re.IGNORECASE if ignore_case else 0,
-                ):
+                if regex.search(value):
                     print_match(
                         f,
                         node,
